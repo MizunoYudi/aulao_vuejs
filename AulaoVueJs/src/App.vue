a40d37e76c04c40ee5daf636fcae792e700c878a
MizunoYudi <joaomizunovy@gmail.com>
2025-10-18 15:51:40 -0300

    Testando componentes com propriedades

--- AulaoVueJs/src/App.vue
+++ AulaoVueJs/src/App.vue
@@ -2,6 +2,7 @@
   import ComDados from "./components/ComDados.vue";
 import ComDadosReativosNaoPrimitivos from "./components/ComDadosReativosNaoPrimitivos.vue";
 import ComDadosReativosPrimitivos from "./components/ComDadosReativosPrimitivos.vue";
+import ComProps from "./components/ComProps.vue";
 import DataBinding from "./components/DataBinding.vue";
 import PrimeiroComponente from "./components/PrimeiroComponente.vue"
 </script>
@@ -13,6 +14,9 @@ import PrimeiroComponente from "./components/PrimeiroComponente.vue"
   <PrimeiroComponente></PrimeiroComponente>
   <DataBinding></DataBinding>
   <ComDados></ComDados>
+  <ComProps item="Cachorro" :preco=2></ComProps>
+  <ComProps item="Cachor" :preco=10></ComProps>
+  <ComProps item="Cacho" :preco=23432432423></ComProps>
 </template>
 
 <style scoped></style>
