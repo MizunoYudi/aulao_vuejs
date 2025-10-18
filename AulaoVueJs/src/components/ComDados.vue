a84416cadb5ed5edb3d16d013a1467e52a4c825e
MizunoYudi <joaomizunovy@gmail.com>
2025-10-18 14:02:34 -0300

    Generalizando estilisação dos componentes

---
+++ AulaoVueJs/src/components/ComDados.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="componente">
+        <h1>Componente com dados</h1>
+        <p>Nome: {{ nomeProduto }} - {{ codigo }}</p>
+    </div>
+</template>
+
+<script setup>
+    const nomeProduto = "Lápis";
+    const codigo = "789"
+
+</script>
+
+<!-- <script>
+// Usando components API
+// export default {
+//     data: function(){
+//         return {
+//             nomeProduto: "Caderno",
+//             codigo: "#321"
+//         };
+//     }
+// }
+
+// Usando composition API
+// Método "Incomum"
+// export default{
+//     setup:function(){
+//         const nomeProduto = "Lápis";
+//         const codigo = "789"
+//         return {nomeProduto, codigo};
+//     }
+// }
+
+</script> -->
